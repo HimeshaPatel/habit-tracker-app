6f90ab24c9143bc88bde7fb87cdce6cf06ac69c1
himesha <himesha.patel7@gmail.com>
2024-12-03 12:59:34 +1100

    frontend complete

--- habit-tracker/src/components/habit-list.tsx
+++ habit-tracker/src/components/habit-list.tsx
@@ -1,14 +1,34 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from '../store/store'
-import { Box, Button, Grid, Paper, Typography } from '@mui/material'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState, AppDispatch } from '../store/store'
+import { Box, Button, Grid,  Paper, Typography } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
+import DeleteIcon  from '@mui/icons-material/Delete'
+import { Habit, toggleHabit } from '../store/habit-slice'
+import LinearProgress from '@mui/material/LinearProgress';
+
 
 const HabitList:React.FC = () => {
-
     const {habits} = useSelector((state: RootState) => state.habits)
+    const dispatch = useDispatch<AppDispatch>();
 
-    const today = new Date().toISOString().split("T")[0]
+    const today = new Date().toISOString().split("T")[0];
+
+    const getStreak = (habit: Habit) => {
+        let streak = 0;
+        const currentDate = new Date();
+        while(true){
+            const dateString = currentDate.toISOString().split("T")[0];
+            if(habit.completedDates.includes(dateString)){
+                streak++;
+                currentDate.setDate(currentDate.getDate() - 1);
+            } else {
+                break;
+            }
+        }
+        console.log("streak", streak);
+        return streak;
+    }
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4}}>
@@ -23,14 +43,26 @@ const HabitList:React.FC = () => {
                     <Box sx={{ display: "flex", gap: 2, justifyContent: "flex-end"}}>  
                         <Button variant='outlined' color={habit.completedDates.includes(today) ? "success" : "primary"}
                         startIcon={<CheckCircle />}
+                        onClick={() => dispatch(toggleHabit({id: habit.id, date: today}))}
                         >Mark Complete</Button>    
+                        <Button variant='outlined' color={"error"}
+                        startIcon={<DeleteIcon />}
+                        >Remove</Button>    
                     </Box>
 
                 </Grid>
             </Grid>
+            <Box sx={{ display: "flex", gap: 2, justifyContent: "flex-start", width: "100%"}}>  
+                <Typography variant="body2">Current Streak: {getStreak(habit)} days</Typography>
+                {/* <LinearProgress variant='determinate' value={(getStreak(habit) / 30) * 100} sx={{ mt: 1 }} /> */}
+            </Box>
+                <LinearProgress variant="determinate" value={(getStreak(habit) / 30) * 100}  sx={{ mt: 1 }} />
+
             </Paper>
+            
         })}
     </Box>
+    
   )
 }
 
